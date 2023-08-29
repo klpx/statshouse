9962803c4b5f47741f030bbbb5e8f5fe9ffd3a5f
e.martyn <dedok1486@gmail.com>
2023-08-29 17:19:39 +0300

    debug uniq

--- cmd/collector_test/collector.go
+++ cmd/collector_test/collector.go
@@ -5,10 +5,37 @@ import (
 	"os"
 	"time"
 
+	"github.com/vkcom/statshouse-go"
 	"github.com/vkcom/statshouse/internal/stats"
+	"pgregory.net/rand"
 )
 
+func tillNextHalfPeriod(now time.Time) time.Duration {
+	return now.Truncate(time.Second).Add(time.Second * 3 / 2).Sub(now)
+}
 func main() {
+
+	m := map[int64]struct{}{}
+	for i := 0; i < 1000000; i++ {
+		m[rand.Int63n(1000000)] = struct{}{}
+	}
+	statshouse.Configure(log.Printf, statshouse.DefaultStatsHouseAddr, "staging")
+	s := []int64{}
+	for {
+		time.Sleep(tillNextHalfPeriod(time.Now()))
+		n := 50 + rand.Intn(50)
+		s = s[:0]
+		i := 0
+		for k := range m {
+			if i == n {
+				break
+			}
+			s = append(s, k)
+			i++
+		}
+		statshouse.Metric("test_uniq", statshouse.Tags{}).Uniques(s)
+	}
+	return
 	host, err := os.Hostname()
 	if err != nil {
 		panic(err)
