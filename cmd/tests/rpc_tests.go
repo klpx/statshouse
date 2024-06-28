324abf8e7606d9c96d95c34b1e40896a149028af
e.martyn <dedok1486@gmail.com>
2024-06-28 15:45:49 +0300

    debig

--- cmd/tests/rpc_tests.go
+++ cmd/tests/rpc_tests.go
@@ -5,41 +5,67 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"time"
 
-	"github.com/vkcom/statshouse/internal/data_model/gen2/tlstatshouseApi"
+	"github.com/vkcom/statshouse/internal/data_model/gen2/tlmetadata"
+	"github.com/vkcom/statshouse/internal/format"
 	"github.com/vkcom/statshouse/internal/vkgo/rpc"
 )
 
 func main() {
-	net := flag.String("statshouse-net", "tcp4", "")
+	net := flag.String("statshouse-net", "unix", "")
 	addr := flag.String("statshouse-addr", "127.0.0.1:13347", "")
-	metric := flag.String("metric", "__heartbeat_version", "")
+	actor := flag.Int("statshouse-actor", 0, "")
+
 	flag.Parse()
 
 	client := rpc.NewClient(rpc.ClientWithLogf(log.Printf))
-	apiClient := tlstatshouseApi.Client{
+	apiClient := tlmetadata.Client{
 		Client:  client,
 		Network: *net,
 		Address: *addr,
+		ActorID: int64(*actor),
 	}
-	to := time.Now().Unix()
-	from := time.Now().Add(-time.Minute * 5).Unix()
-	qp := tlstatshouseApi.GetQueryPoint{
-		AccessToken: "",
-		Query: tlstatshouseApi.QueryPoint{
-			Version:    2,
-			TopN:       1,
-			MetricName: *metric,
-			TimeFrom:   from,
-			TimeTo:     to,
-			Function:   tlstatshouseApi.FnCount(),
-		},
+	for i := 50000; i < 100000; i++ {
+		event := tlmetadata.Event{}
+		err := apiClient.EditEntitynew(context.Background(), tlmetadata.EditEntitynew{
+			FieldsMask: 0,
+			Event: tlmetadata.Event{
+				Id:        format.PrometheusConfigID,
+				Name:      "prom-config",
+				EventType: format.PromConfigEvent,
+				Version:   int64(i),
+				Data:      "",
+			},
+		}, nil, &event)
+		if err == nil {
+			fmt.Println("SAVE1", event.Version)
+			break
+		}
 	}
-	resp := tlstatshouseApi.GetQueryPointResponse{}
-	err := apiClient.GetQueryPoint(context.Background(), qp, nil, &resp)
-	if err != nil {
-		panic(err)
+	for i := 50000; i < 100000; i++ {
+		event := tlmetadata.Event{}
+		err := apiClient.EditEntitynew(context.Background(), tlmetadata.EditEntitynew{
+			FieldsMask: 0,
+			Event: tlmetadata.Event{
+				Id:        format.PrometheusGeneratedConfigID,
+				Name:      "prom-static-config",
+				EventType: format.PromConfigEvent,
+				Version:   int64(i),
+				Data:      "",
+			},
+		}, nil, &event)
+		if err == nil {
+			fmt.Println("SAVE2", event.Version)
+			break
+		}
 	}
-	fmt.Println(string(resp.WriteJSON(nil)))
+	//if err != nil {
+	//	panic(err)
+	//	err := apiClient.GetEntity(context.Background(), tlmetadata.GetEntity{
+	//		Id:      format.PrometheusGeneratedConfigID,
+	//		Version: int64(i),
+	//	}, nil, &event)
+	//	if err == nil {
+	//		fmt.Println("VERSION IS", event.Version)
+	//	}
 }
